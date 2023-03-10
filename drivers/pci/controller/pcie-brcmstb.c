19780cd0db435afaaf39fa0ab13c70aaa855e2b3
Philipp Rosenberger <code@iluminat23.org>
2023-03-10 12:02:59 +0100

    fixup! add debug msgs for pcie sleep

--- drivers/pci/controller/pcie-brcmstb.c
+++ drivers/pci/controller/pcie-brcmstb.c
@@ -959,15 +959,6 @@ static int brcm_pcie_setup(struct brcm_pcie *pcie)
 	msleep(100);
 	/* Unassert the fundamental reset */
 	pcie->perst_set(pcie, 0);
-	msleep(100);
-	pcie->perst_set(pcie, 1);
-	msleep(100);
-	pcie->perst_set(pcie, 0);
-	msleep(100);
-	pcie->perst_set(pcie, 1);
-	msleep(100);
-	pcie->perst_set(pcie, 0);
-	msleep(100);
 
 	/*
 	 * Wait for 100ms after PERST# deassertion; see PCIe CEM specification
