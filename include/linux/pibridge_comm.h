2eb00e638c7e8ca826850622e465ad7cf0e01322
Lino Sanfilippo <l.sanfilippo@kunbus.com>
2025-10-06 14:59:32 +0200

    pibridge-serdev: introduce function to send and receive complete datagrams
    Introduce function pibridge_req_gate_datagram() which can be used to send
    a complete datagram which is provided by the caller. The response is stored
    in a response datagram, if available.
    This makes it possible to have control over the header attributes like the
    sequence number. It also allows to create invalid datagrams which can be
    used to test the RevPi modules connected to the PiBridge.
    
    Since a whole datagram also includes the header, make the gate datagram
    header available by moving it from pibridge.h to pibridge_comm.h.
    Also provide the structure for a complete datagram in this header file.
    
    Signed-off-by: Lino Sanfilippo <l.sanfilippo@kunbus.com>

--- include/linux/pibridge_comm.h
+++ include/linux/pibridge_comm.h
@@ -3,9 +3,28 @@
 
 #include <linux/types.h>
 
+struct pibridge_pkthdr_gate {
+	u8	dst;
+	u8	src;
+	u16	cmd;
+	u16	seq;
+	u8	len;
+} __attribute__((packed));
+
+#define PIBRIDGE_MAX_GATE_DATA		255
+#define PIBRIDGE_CRC_LEN		1
+
+struct pibridge_gate_datagram {
+	struct pibridge_pkthdr_gate hdr;
+	u8 data[PIBRIDGE_MAX_GATE_DATA + PIBRIDGE_CRC_LEN];
+} __attribute__((packed));
+
+
 int pibridge_req_io(u8 addr, u8 cmd, void *snd_buf, u8 snd_len, void *rcv_buf,
 		    u8 rcv_len);
 int pibridge_req_send_io(u8 addr, u8 cmd, void *snd_buf, u8 snd_len);
+int pibridge_req_gate_datagram(struct pibridge_gate_datagram *req,
+			       struct pibridge_gate_datagram *resp);
 int pibridge_req_gate(u8 dst, u16 cmd, void *snd_buf, u8 snd_len,
 		      void *rcv_buf, u8 rcv_len);
 int pibridge_req_gate_tmt(u8 dst, u16 cmd, void *snd_buf, u8 snd_len,
